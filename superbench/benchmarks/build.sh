d3bd2dca6df5ef586ae0cc0b58c5d5a02ea5d95a
Yaoming Mu <yaoming.mu@amd.com>
2026-02-09 17:46:23 -0600

    disable build gpu_stream because nvml is for NVIDIA Management Library, for AMD, need to use AMD Data Center GPU management adm-smi/rocm-smi

--- superbench/benchmarks/build.sh
+++ superbench/benchmarks/build.sh
@@ -9,12 +9,14 @@ MPI_HOME="${MPI_HOME:-/usr/local/mpi}"
 SB_MICRO_PATH="${SB_MICRO_PATH:-/usr/local}"
 
 for dir in micro_benchmarks/*/ ; do
-    if [ -f $dir/CMakeLists.txt ]; then
-        SOURCE_DIR=$dir
-        BUILD_ROOT=$dir/build
-        mkdir -p $BUILD_ROOT
-        cmake -DCMAKE_PREFIX_PATH=$MPI_HOME -DCMAKE_INSTALL_PREFIX=$SB_MICRO_PATH -DCMAKE_BUILD_TYPE=Release -S $SOURCE_DIR -B $BUILD_ROOT
-        cmake --build $BUILD_ROOT
-        cmake --install $BUILD_ROOT
-    fi
+    if [ $dir != "micro_benchmarks/gpu_stream/" ] || [ $CXX != "/opt/rocm/bin/hipcc" ];  then	
+    	if [ -f $dir/CMakeLists.txt ]; then
+        	SOURCE_DIR=$dir
+        	BUILD_ROOT=$dir/build
+        	mkdir -p $BUILD_ROOT
+        	cmake -DCMAKE_PREFIX_PATH=$MPI_HOME -DCMAKE_INSTALL_PREFIX=$SB_MICRO_PATH -DCMAKE_BUILD_TYPE=Release -S $SOURCE_DIR -B $BUILD_ROOT
+        	cmake --build $BUILD_ROOT
+        	cmake --install $BUILD_ROOT
+    	fi
+    fi	
 done
